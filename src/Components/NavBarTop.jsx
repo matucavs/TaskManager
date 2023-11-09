31bb4ab904354e35b3f77c29dc4bf1c05039b763
Matias <foos@live.com.ar>
2023-11-09 17:00:56 -0300

    second-commit
    Co-authored-by: Francisco Rodriguez <Franrz2023@users.noreply.github.com>

---
+++ src/Components/NavBarTop.jsx
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavbarTop(){
+  return (
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#f">Productos</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+}
+
+export default NavbarTop;
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,23 @@
+// Componente de Lista de Tareas (TaskList):
+// Este componente deberá mostrar la lista de tareas.
+// Recibirá como propiedades la lista de tareas y funciones para gestionar eventos
+// relacionados con las tareas (por ejemplo, marcar como completada, eliminar, etc.).
+// Cada tarea debe representarse mediante un componente TaskItem.
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <>
+    <div className="bg-primary">
+    <h1 >
+        Lista De Tareas
+    </h1>
+    </div>
+
+    </>
+
+  )
+}
+
+export default TaskList
